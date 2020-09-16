eb37b4ca7e45eb77247d3128de39611246f59460
Tim Kotowski <tkotowski7@gmail.com>
2020-09-16 10:20:06 -0500

    done

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-		"encoding/json"
-    "fmt"
-    "net/http"
-    "log"
-		"database/sql"
-
-		"github.com/julienschmidt/httprouter"
-		_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
 )
 
-
 type Todo struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-	Title string `json:"title"`
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
 }
 
@@ -23,125 +21,123 @@ var db *sql.DB
 
 func GetAll(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		// Query MySQL to get all the todos from the todos table.
-		results, err := db.Query("SELECT * FROM todos")
-		 if err != nil {
-			 log.Println(err)
-			 w.Write([]byte("We got an error"))
-			 return
-		 }
-		 defer results.Close()
+	// Query MySQL to get all the todos from the todos table.
+	results, err := db.Query("SELECT * FROM todos")
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("We got an error"))
+		return
+	}
+	defer results.Close()
 
-		 // Make a variable to hold all of our todos.
-		 todos := []Todo{}
+	// Make a variable to hold all of our todos.
+	todos := []Todo{}
 
-		 	// loop through the results aka the row with .Next()
-		 for results.Next(){
-			 // crate a todo variable to refernce the Todo struct
-			todo := Todo{}
-			err = results.Scan(&todo.ID, &todo.Name, &todo.Title, &todo.Description)
-			if err != nil {
-				log.Println(err)
-				w.Write([]byte("We got an error"))
-				return
-			}
-
-			// Place new todo inside our slice of todos.
-			todos = append(todos, todo)
-		 }
-
-		 // Now that we have our slice of todos, let's write
-		 // back a response of all the todos as JSON.
-		 jsonBytes, err := json.Marshal(todos)
-		 if err != nil {
+	// loop through the results aka the row with .Next()
+	for results.Next() {
+		// crate a todo variable to refernce the Todo struct
+		todo := Todo{}
+		err = results.Scan(&todo.ID, &todo.Name, &todo.Title, &todo.Description)
+		if err != nil {
 			log.Println(err)
 			w.Write([]byte("We got an error"))
 			return
-		 }
-
-		 // Set Content-Type header to signify we're sending back
-		 // JSON, and write out the JSON.
-		 fmt.Println("json", string(jsonBytes))
-		 w.Write(jsonBytes)
 		}
-
-func SingleTodo(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
-	w.Header().Set("Content Type", "application/json; charset=utf-8")
-		// fmt.Fprintf(w, "hello, %s!\n", ps.ByName("id"))
-		params := ps.ByName("id")
-
-		todo := Todo{}
-		selectedTodo := `SELECT id, name, title, description FROM todos WHERE id=?`
-		row := db.QueryRow(selectedTodo, params)
-
-		err := row.Scan(&todo.ID, &todo.Name, &todo.Title, &todo.Description)
-		if err != nil {
-			fmt.Println(err)
-			w.Write([]byte("we got an error"))
-			return
-		}
-			jsonData, err := json.Marshal(todo)
-
-			if err != nil {
-				log.Println(err)
-				w.Write([]byte("we got an error"))
-				return
-			}
-			// string converts the bytes to the string representiation aka value
-			fmt.Println("json", string(jsonData))
-			w.Write(jsonData)
+		// Place new todo inside our slice of todos.
+		todos = append(todos, todo)
 	}
 
+	// Now that we have our slice of todos, let's write
+	// back a response of all the todos as JSON.
+	jsonBytes, err := json.Marshal(todos)
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("We got an error"))
+		return
+	}
 
-func CreateTodo(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			var reqBody Todo
-			if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-				log.Println(err)
-				w.Write([]byte("bad request"))
-				return
-			}
-
-			// log out the body
-			fmt.Printf("body %+v", reqBody)
-
-			insertStatment := `INSERT INTO todos (name, title, description) VALUES (?, ?, ?)`
-			_, err := db.Exec(insertStatment, reqBody.Name, reqBody.Title, reqBody.Description)
-
-			jsonData, err := json.Marshal(reqBody)
-			fmt.Println("row", jsonData)
-
-			if err != nil {
-				log.Println(err)
-				w.Write([]byte("We got an error"))
-				return
-			}
-			// Set Content-Type header to signify we're sending back
-			// JSON, and write out the JSON.
-			fmt.Println("json", string(jsonData))
-			// res.json equzlivelnt
-			w.Write(jsonData)
-		}
-
-func DeleteTodo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-			deleteTodo := `DELETE FROM todos WHERE id=?`
-			// store a varible with the struct
-			// get the param value of the oroute
-			params := ps.ByName("id")
-			// satrt the qury aosing in the deletetodo and the params
-			// be reeutrned in the row
-			_, err := db.Exec(deleteTodo, params)
-			if err != nil {
-				log.Println(err)
-				w.Write([]byte("we got an error"))
-				return
-			}
-				w.Write([]byte("deleted"))
+	// Set Content-Type header to signify we're sending back
+	// JSON, and write out the JSON.
+	fmt.Println("json", string(jsonBytes))
+	w.Write(jsonBytes)
 }
 
-func UpdateTodo(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+func SingleTodo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content Type", "application/json; charset=utf-8")
+	// fmt.Fprintf(w, "hello, %s!\n", ps.ByName("id"))
+	params := ps.ByName("id")
+
+	todo := Todo{}
+	selectedTodo := `SELECT id, name, title, description FROM todos WHERE id=?`
+	row := db.QueryRow(selectedTodo, params)
+
+	err := row.Scan(&todo.ID, &todo.Name, &todo.Title, &todo.Description)
+	if err != nil {
+		fmt.Println(err)
+		w.Write([]byte("we got an error"))
+		return
+	}
+	jsonData, err := json.Marshal(todo)
+
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("we got an error"))
+		return
+	}
+	// string converts the bytes to the string representiation aka value
+	fmt.Println("json", string(jsonData))
+	w.Write(jsonData)
+}
+
+func CreateTodo(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	var reqBody Todo
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		log.Println(err)
+		w.Write([]byte("bad request"))
+		return
+	}
+
+	// log out the body
+	fmt.Printf("body %+v", reqBody)
+
+	insertStatment := `INSERT INTO todos (name, title, description) VALUES (?, ?, ?)`
+	_, err := db.Exec(insertStatment, reqBody.Name, reqBody.Title, reqBody.Description)
+
+	jsonData, err := json.Marshal(reqBody)
+	fmt.Println("row", jsonData)
+
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("We got an error"))
+		return
+	}
+	// Set Content-Type header to signify we're sending back
+	// JSON, and write out the JSON.
+	fmt.Println("json", string(jsonData))
+	// res.json equzlivelnt
+	w.Write(jsonData)
+}
+
+func DeleteTodo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	deleteTodo := `DELETE FROM todos WHERE id=?`
+	// store a varible with the struct
+	// get the param value of the oroute
+	params := ps.ByName("id")
+	// satrt the qury aosing in the deletetodo and the params
+	// be reeutrned in the row
+	_, err := db.Exec(deleteTodo, params)
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("we got an error"))
+		return
+	}
+	w.Write([]byte("deleted"))
+}
+
+func UpdateTodo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
 	updateTodo := `UPDATE todos SET name=?, title=?, description =? WHERE id=?`
@@ -172,17 +168,17 @@ func main() {
 	}
 
 	defer db.Close()
-		// Ping check were connected
-		db.Ping()
-		fmt.Println("connected!")
+	// Ping check were connected
+	db.Ping()
+	fmt.Println("connected!")
 
-    router := httprouter.New()
-		router.GET("/api/todos", GetAll)
-		router.GET("/api/todo/:id", SingleTodo)
-		router.POST("/api/create/todos", CreateTodo)
-		router.PUT("/api/update/:id", UpdateTodo)
-		router.DELETE("/api/delete/:id", DeleteTodo)
+	router := httprouter.New()
+	router.GET("/api/todos", GetAll)
+	router.GET("/api/todo/:id", SingleTodo)
+	router.POST("/api/create/todos", CreateTodo)
+	router.PUT("/api/update/:id", UpdateTodo)
+	router.DELETE("/api/delete/:id", DeleteTodo)
 
-		fmt.Println("Server running on port 8080...")
-		log.Fatal(http.ListenAndServe(":8080", router))
+	fmt.Println("Server running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
